242f6dee206f80aed0e6273af45b51fa93d07a59
Devon Roepke <DevonRoepke1@Gmail.com>
2021-09-17 09:03:56 -0500

    Updated Google Analytics

--- src/useGaTracker.js
+++ src/useGaTracker.js
@@ -7,9 +7,7 @@ const useGaTracker = () => {
     const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
-        if (!window.location.href.includes("localhost")) {
         ReactGA.initialize("UA-207738261-2");
-        }
         setInitialized(true);
     }, []);
 
